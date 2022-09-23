7f0495303656a8e82fa89644b8c75e4df99d118d
austindonnelly <austindonnelly3@gmail.com>
2022-09-23 13:54:06 +1200

    quiz successfully working with the one single maths question. Made as a database so there is always ways to make the quiz contain more questions etc. Need to get it so the quiz is doing something when a user fails to answer a question successfully.

--- app/src/main/java/com/example/alarmapp/AlarmActivity.kt
+++ app/src/main/java/com/example/alarmapp/AlarmActivity.kt
@@ -1,123 +1,157 @@
 package com.example.alarmapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
-import com.example.alarmapp.databinding.ActivityAlarmBinding
+import kotlinx.android.synthetic.main.activity_alarm.*
 
-class AlarmActivity : AppCompatActivity() {
+class AlarmActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var _binding: ActivityAlarmBinding? = null
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<QuestionDatabase>? = null
+    private var mSelectedOptionPosition: Int = 0
 
-    private val binding get() = _binding!!
-    private var questionPos:Int=1
-    private var questionList:ArrayList<QuestionDatabase> ? = null
-    private var selecedOption:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
 
-        questionList=AddToDatabase.getQuestion()
-
+        mQuestionList = AddToDatabase.getQuestion()
         setQuestion()
 
-        binding.opt1.setOnClickListener{
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+        btn_submit.setOnClickListener(this)
+    }
 
-            selectedOptionStyle(binding.opt1,1)
+    @SuppressLint("SetTextI18n")
+    private fun setQuestion() {
+
+        val question = mQuestionList!!.get(mCurrentPosition - 1)
+
+        defaultOptionsView()
+        if (mCurrentPosition == mQuestionList!!.size) {
+            btn_submit.text = "Finish"
+        } else {
+            btn_submit.text = "Submit"
         }
-        binding.opt2.setOnClickListener{
 
-            selectedOptionStyle(binding.opt2,2)
+        tv_question.text = question.question
+        tv_option_one.text = question.option_one
+        tv_option_two.text = question.option_two
+        tv_option_three.text = question.option_three
+        tv_option_four.text = question.option_four
+    }
+
+    private fun defaultOptionsView() {
+
+        val options = ArrayList<TextView>()
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3, tv_option_four)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.option_for_question
+            )
         }
-        binding.opt3.setOnClickListener{
 
-            selectedOptionStyle(binding.opt3,3)
-        }
-        binding.opt4.setOnClickListener{
+    }
 
-            selectedOptionStyle(binding.opt4,4)
-        }
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tv_option_one -> {
+                selectedOptionView(tv_option_one, 1)
+            }
+            R.id.tv_option_two -> {
+                selectedOptionView(tv_option_two, 2)
+            }
+            R.id.tv_option_three -> {
+                selectedOptionView(tv_option_three, 3)
+            }
+            R.id.tv_option_four -> {
+                selectedOptionView(tv_option_four, 4)
+            }
+            R.id.btn_submit -> {
+                if (mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
 
-        binding.submit.setOnClickListener {
-            if(selecedOption!=0)
-            {
-                val question=questionList!![questionPos-1]
-                if(selecedOption!=question.correct_ans)
-                {
-                    setColor(selecedOption,R.drawable.incorrect_answer)
-                }else{
-                    var intent= Intent(this,QuestionResult::class.java)
-                    startActivity(intent)
-                    finish()
+                    when {
+                        mCurrentPosition <= mQuestionList!!.size -> {
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(
+                                this,
+                                "Rise&Shine!!", Toast.LENGTH_SHORT
+                            ).show()
+                            val intent = Intent(this,MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                } else {
+                    val question = mQuestionList?.get(mCurrentPosition - 1)
+                    if (question!!.correct_ans != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.incorrect_answer)
+                    }
+                    answerView(question.correct_ans, R.drawable.correct_answer)
+                    if (mCurrentPosition == mQuestionList!!.size) {
+                        btn_submit.text = "Finish"
+                    } else {
+                        btn_submit.text = "Go to next question"
+                    }
+                    mSelectedOptionPosition = 0
                 }
-                setColor(question.correct_ans,R.drawable.correct_answer)
-                if(questionPos==questionList!!.size)
-                    binding.submit.text="FINISH"
-                else
-                    binding.submit.text="Go to Next"
-            }
-            selecedOption=0
-        }
 
-    }
-
-    fun setColor(opt:Int,color:Int){
-        when(opt){
-            1->{
-                binding.opt1.background=ContextCompat.getDrawable(this,color)
-            }
-            2->{
-                binding.opt2.background=ContextCompat.getDrawable(this,color)
-            }
-            3->{
-                binding.opt3.background=ContextCompat.getDrawable(this,color)
-            }
-            4->{
-                binding.opt4.background=ContextCompat.getDrawable(this,color)
             }
         }
     }
 
-
-    fun setQuestion(){
-
-        val question = questionList!![questionPos-1]
-        setOptionStyle()
-
-        binding.questionText.text=question.question
-        binding.opt1.text=question.option_one
-        binding.opt2.text=question.option_tw0
-        binding.opt3.text=question.option_three
-        binding.opt4.text=question.option_four
-
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option
+        )
     }
 
-    fun setOptionStyle(){
-        var optionList:ArrayList<TextView> = arrayListOf()
-        optionList.add(0,binding.opt1)
-        optionList.add(1,binding.opt2)
-        optionList.add(2,binding.opt3)
-        optionList.add(3,binding.opt4)
-
-        for(op in optionList)
-        {
-            op.setTextColor(Color.parseColor("#555151"))
-            op.background=ContextCompat.getDrawable(this,R.drawable.option_for_question)
-            op.typeface= Typeface.DEFAULT
+    private fun answerView(answer: Int, drawableView: Int) {
+        when (answer) {
+            1 -> {
+                tv_option_one.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            2 -> {
+                tv_option_two.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            3 -> {
+                tv_option_three.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            4 -> {
+                tv_option_four.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
         }
-    }
-
-    fun selectedOptionStyle(view:TextView,opt:Int){
-
-        setOptionStyle()
-        selecedOption=opt
-        view.background=ContextCompat.getDrawable(this,R.drawable.selected_option)
-        view.typeface= Typeface.DEFAULT_BOLD
-        view.setTextColor(Color.parseColor("#000000"))
-
     }
 }
--- app/src/main/java/com/example/alarmapp/QuestionDatabase.kt
+++ app/src/main/java/com/example/alarmapp/QuestionDatabase.kt
@@ -5,7 +5,7 @@ var id:Int,
 var question:String,
 
 var option_one:String,
-var option_tw0:String,
+var option_two:String,
 var option_three:String,
 var option_four:String,
 var correct_ans:Int
