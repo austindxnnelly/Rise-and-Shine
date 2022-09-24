e684baad32167526d488607d54a3d6e3fc90f930
shaystevens <shaystevns@gmail.com>
2022-09-24 19:39:13 +1200

    Deleted main activity tests

--- app/src/test/java/com/example/alarmapp/MainActivityTest.kt
+++ app/src/test/java/com/example/alarmapp/MainActivityTest.kt
@@ -4,11 +4,4 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class MainActivityTest{
-    @Test
-    fun testMainActivity(){
-        val mainActivity = MainActivity()
-        mainActivity.createNotificationChannel()
-        mainActivity.setAlarm(0, 0, 0)
-        mainActivity.cancelAlarm(0)
-    }
 }
