1380bce731960beb3058a57d09b3c2e8a9a07d63
shaystevens <shaystevns@gmail.com>
2022-09-24 17:17:08 +1200

    Edit dialog unit tests

--- app/src/test/java/com/example/alarmapp/EditDialogTest.kt
+++ app/src/test/java/com/example/alarmapp/EditDialogTest.kt
@@ -1,5 +1,8 @@
 package com.example.alarmapp
 
+import android.view.View
+import kotlinx.android.synthetic.main.edit_dialog.*
+import kotlinx.android.synthetic.main.my_row.*
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -14,5 +17,41 @@ class EditDialogTest{
             "00:00",
             true).hourToMilliSecond(10)
         assertEquals(result, 10*3600000)
+    }
+
+    @Test
+    fun checkMinuteToMilliSecond(){
+        val result = EditDialog(
+            "0",
+            "test",
+            0,
+            0,
+            "00:00",
+            true).minuteToMilliSecond(10)
+        assertEquals(result, 10*60000)
+    }
+
+    @Test
+    fun checkSecondToMilliSecond(){
+        val result = EditDialog(
+            "0",
+            "test",
+            0,
+            0,
+            "00:00",
+            true).secondToMilliSecond(10)
+        assertEquals(result, 10*1000)
+    }
+
+    @Test
+    fun checkView(){
+        val result = EditDialog(
+            "0",
+            "test",
+            0,
+            0,
+            "00:00",
+            true).view
+        assertNotNull(result)
     }
 }
