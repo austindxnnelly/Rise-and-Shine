cc6b1c42b0e9d97801f02d679dfff60524a1451e
shaystevens <shaystevns@gmail.com>
2022-09-24 19:19:09 +1200

    Deleted database context

--- app/src/test/java/com/example/alarmapp/AlarmDatabaseTest.kt
+++ app/src/test/java/com/example/alarmapp/AlarmDatabaseTest.kt
@@ -7,13 +7,5 @@ import org.junit.Test
 class AlarmDatabaseTest{
     @Test
     fun testDatabase(){
-        val appContext = InstrumentationRegistry.getInstrumentation().context
-
-        val db = AlarmDatabase(appContext, "TestDatabase", null, 1)
-        db.addAlarm("test", 0, 0, 0)
-        val cursor = db.readAllData()
-        val id = cursor?.getInt(0)
-        db.updateDatabase(id.toString(), "test_change", 1, 1, 1)
-        db.deleteOneRow(id.toString())
     }
 }
