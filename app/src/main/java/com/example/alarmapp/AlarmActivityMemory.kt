7de991986bd22e57ea3a7511290e5196a6e787ac
austindonnelly <austindonnelly3@gmail.com>
2022-10-07 22:00:16 +1300

    memory game implemented

--- app/src/main/java/com/example/alarmapp/AlarmActivityMemory.kt
+++ app/src/main/java/com/example/alarmapp/AlarmActivityMemory.kt
@@ -1,177 +1,100 @@
 package com.example.alarmapp
-
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.Typeface
-import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_alarm_math.*
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_alarm_memory.*
 
-/**
- * Alarm activity class, used for the alarm question to disable the alarm.
- *
- * @author Shay Stevens, Dougal Colquhoun, Liam Iggo, Austin Donnelly
- */
-class AlarmActivityMemory : AppCompatActivity(), View.OnClickListener {
+private const val TAG = "AlarmActivityMemory"
 
-    private var mCurrentPosition: Int = 1
-    private var mQuestionList: ArrayList<QuestionDatabase>? = null
-    private var mSelectedOptionPosition: Int = 0
+class AlarmActivityMemory : AppCompatActivity() {
+
+    private lateinit var buttons: List<ImageButton>
+    private lateinit var cards: List<MemoryCard>
+    private var indexOfSingleSelectedCard: Int? = null
     private var correctAmt: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alarm_math)
+        setContentView(R.layout.activity_alarm_memory)
 
-        mQuestionList = AddToDatabase.getQuestion()
-        setQuestion()
+        val images = mutableListOf(R.drawable.apple, R.drawable.orange, R.drawable.pear, R.drawable.berry)
+        images.addAll(images)
+        images.shuffle()
 
-        tv_option_one.setOnClickListener(this)
-        tv_option_two.setOnClickListener(this)
-        tv_option_three.setOnClickListener(this)
-        tv_option_four.setOnClickListener(this)
-        btn_submit.setOnClickListener(this)
+        buttons = listOf(button1, button2, button3, button4, button5,
+            button6, button7, button8)
+
+        cards = buttons.indices.map { index ->
+            MemoryCard(images[index])
+        }
+
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                Log.i(TAG, "Option Selected.")
+                updateModels(index)
+                updateViews()
+            }
+        }
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun setQuestion() {
+    private fun updateViews() {
+        cards.forEachIndexed { index, card ->
+            val button = buttons[index]
+            if (card.isMatched) {
+                button.alpha = 0.1f
+            }
+            button.setImageResource(if (card.isFaceUp) card.identifier else R.drawable.facedown)
+        }
+    }
 
-        val question = mQuestionList!!.get(mCurrentPosition - 1)
+    private fun updateModels(position: Int) {
+        val card = cards[position]
+        if (card.isFaceUp) {
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-        defaultOptionsView()
-        if (mCurrentPosition == mQuestionList!!.size) {
-            btn_submit.text = "Finish"
+        if (indexOfSingleSelectedCard == null) {
+            restoreCards()
+            indexOfSingleSelectedCard = position
         } else {
-            btn_submit.text = "Submit"
+            checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
         }
-
-        tv_question.text = question.question
-        tv_option_one.text = question.option_one
-        tv_option_two.text = question.option_two
-        tv_option_three.text = question.option_three
-        tv_option_four.text = question.option_four
+        card.isFaceUp = !card.isFaceUp
     }
 
-    private fun defaultOptionsView() {
-
-        val options = ArrayList<TextView>()
-        options.add(0, tv_option_one)
-        options.add(1, tv_option_two)
-        options.add(2, tv_option_three)
-        options.add(3, tv_option_four)
-
-        for (option in options) {
-            option.setTextColor(Color.parseColor("#7A8089"))
-            option.typeface = Typeface.DEFAULT
-            option.background = ContextCompat.getDrawable(
-                this,
-                R.drawable.option_for_question
-            )
-        }
-
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.tv_option_one -> {
-                selectedOptionView(tv_option_one, 1)
-            }
-            R.id.tv_option_two -> {
-                selectedOptionView(tv_option_two, 2)
-            }
-            R.id.tv_option_three -> {
-                selectedOptionView(tv_option_three, 3)
-            }
-            R.id.tv_option_four -> {
-                selectedOptionView(tv_option_four, 4)
-            }
-            R.id.btn_submit -> {
-                if (mSelectedOptionPosition == 0) {
-                    mCurrentPosition++
-
-                    when {
-                        mCurrentPosition <= mQuestionList!!.size -> {
-                            setQuestion()
-                        }
-                        else -> {
-                            Toast.makeText(
-                                this,
-                                "Rise&Shine!!", Toast.LENGTH_SHORT
-                            ).show()
-                            val intent = Intent(this,MainActivity::class.java)
-                            startActivity(intent)
-                        }
-                    }
-                } else {
-                    val question = mQuestionList?.get(mCurrentPosition - 1)
-                    if (question!!.correct_ans != mSelectedOptionPosition) {
-                        answerView(mSelectedOptionPosition, R.drawable.incorrect_answer)
-                    } else {
-                        answerView(question.correct_ans, R.drawable.correct_answer)
-                        correctAmt += 1;
-                    }
-                    if (mCurrentPosition == mQuestionList!!.size) {
-                        btn_submit.text = "Finish"
-                        //Where the alarm is stopped (not correct place)
-                        stopAlarm()
-                    } else {
-                        btn_submit.text = "Go to next question"
-                    }
-                    mSelectedOptionPosition = 0
-                }
-
+    private fun restoreCards() {
+        for (card in cards) {
+            if (!card.isMatched) {
+                card.isFaceUp = false
             }
         }
     }
 
-    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
-        defaultOptionsView()
-        mSelectedOptionPosition = selectedOptionNum
-        tv.setTextColor(Color.parseColor("#363A43"))
-        tv.setTypeface(tv.typeface, Typeface.BOLD)
-        tv.background = ContextCompat.getDrawable(
+    private fun checkForMatch(position1: Int, position2: Int) {
+        if (cards[position1].identifier == cards[position2].identifier) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+            cards[position1].isMatched = true
+            cards[position2].isMatched = true
+            correctAmt += 1;
+            if (correctAmt == 4) {
+                stopAlarm();
+            }
+        }
+    }
+
+    private fun stopAlarm() {
+        val intentService = Intent(applicationContext, AlarmService::class.java)
+        applicationContext.stopService(intentService)
+        Toast.makeText(
             this,
-            R.drawable.selected_option
-        )
-    }
-
-    private fun answerView(answer: Int, drawableView: Int) {
-        when (answer) {
-            1 -> {
-                tv_option_one.background = ContextCompat.getDrawable(
-                    this, drawableView
-                )
-            }
-            2 -> {
-                tv_option_two.background = ContextCompat.getDrawable(
-                    this, drawableView
-                )
-            }
-            3 -> {
-                tv_option_three.background = ContextCompat.getDrawable(
-                    this, drawableView
-                )
-            }
-            4 -> {
-                tv_option_four.background = ContextCompat.getDrawable(
-                    this, drawableView
-                )
-            }
-        }
-    }
-
-    private fun stopAlarm(){
-        if (correctAmt == 2) {
-            val intentService = Intent(applicationContext, AlarmService::class.java)
-            applicationContext.stopService(intentService)
-        } else {
-            val intent = Intent(this,AlarmActivity::class.java)
-            startActivity(intent)
-        }
+            "Rise&Shine!!", Toast.LENGTH_SHORT
+        ).show()
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
